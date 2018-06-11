a45468a5ad09567540724d28eed240f12ba945a0
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-06-11 19:23:19 +0000

    ap ptesting

--- README.md
+++ README.md
@@ -115,9 +115,15 @@ Password: testtest
 
 ## Test Suite:
 
-### Travis CI
+### Travis CI:
 
 [![Build Status](https://travis-ci.org/LukaszMalucha/DCD-Milestone-Project.svg?branch=master)](https://travis-ci.org/LukaszMalucha/DCD-Milestone-Project)
+
+### Test Files:
+
+##### test.py
+
+### Other Tests:
 
 #### Login/Signin Form Test:
 1. Is data properly saved in datbase
--- test.py
+++ test.py
@@ -1,10 +1,102 @@
-from app import app
+from app import app, User
 import unittest
 from flask_login import current_user
 
-class FlaskTestCase(unittest.TestCase):
+class UsersTestCase(unittest.TestCase):
     
-        def test_index(self):
+        def test_login(self):
             tester = app.test_client(self)
-            response = tester.get('/dashboard', content_type='html/text')
+            response = tester.get('/login', content_type='html/text')
             self.assertEqual(response.status_code, 200)
+            
+        # Login page loads correctly  
+        def test_login_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/login', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'Please Log in' in response.data)     
+            
+        def test_correct_signup(self):
+            tester = app.test_client(self)
+            response = tester.post('/signup', data=dict(username="admin", 
+                                                        email="admin@gmail.com", 
+                                                        password="adminadmin"), 
+                                                        follow_redirects = True)
+            self.assertTrue(b'sign' in response.data)         
+            
+        # Correct login credentials    
+        def test_correct_login(self):
+            tester = app.test_client(self)
+            response = tester.post('/login', data=dict(username="admin", password="adminadmin"), 
+                                                follow_redirects = True)
+            self.assertTrue(b'Please Log in' in response.data)     
+
+        # Login works correctly given incorrect credentials    
+        def test_incorrect_login(self):
+            tester = app.test_client(self)
+            response = tester.post('/login', data=dict(username="admin", password="zzz"), 
+                                                follow_redirects = True)
+            self.assertTrue(b'Field must be between 6' in response.data)               
+            
+        # Ensure that main page @logout_required    
+        def test_logout_requires_login(self):
+            tester = app.test_client(self)
+            response = tester.get('/logout', follow_redirects = True)
+            self.assertTrue(b'Please Log in' in response.data)    
+            
+            
+class AlgorithmsTestCase(unittest.TestCase):
+
+            
+
+        # Classification Page Test
+        def test_classification_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/classification', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'classification' in response.data) 
+            
+        # Regression Page Test    
+        def test_regression_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/regression', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'regression' in response.data) 
+            
+        # Clustering Page Test    
+        def test_clustering_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/clustering', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'clustering' in response.data) 
+
+
+
+class LibraryTestCase(unittest.TestCase):
+
+            
+        # Classification Page Test
+        def test_add_request_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/add_request', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'Add Template' in response.data) 
+            
+        # Regression Page Test    
+        def test_edit_code_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/edit_code/1', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'Edit Template' in response.data) 
+            
+        # Clustering Page Test    
+        def test_library_page_loads(self):
+            tester = app.test_client(self)
+            response = tester.get('/', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'Available Code Templates' in response.data) 
+     
+            
+
+if __name__ == '__main__':
+    unittest.main()            
